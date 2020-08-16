5c2e4eaf9a83818ae05a4b3c41dc18178f16c65e
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2020-08-16 16:56:03 +0100

    Iteration 1 - Core Feature: Character Has Abilities Scores

--- EverCraftKata/TypeScript/src/character.ts
+++ EverCraftKata/TypeScript/src/character.ts
@@ -2,13 +2,23 @@ import { Alignment } from './alignment'
 import { Combatant } from './combatant'
 import { D20 } from './dice'
 import { AttackOutcome } from './attack-outcome'
+import { AbilityScore } from './ability-score'
 
 export class Character implements Combatant {
 
   // TODO: (not in spec) add validation to prevent character name being empty;
   public Name = '';
   public Alignment: Alignment = Alignment.Neutral;
+
+  public Strength: AbilityScore = AbilityScore.Ten;
+  public Dexterity: AbilityScore = AbilityScore.Ten;
+  public Constitution: AbilityScore = AbilityScore.Ten;
+  public Wisdom: AbilityScore = AbilityScore.Ten;
+  public Intelligence: AbilityScore = AbilityScore.Ten;
+  public Charisma: AbilityScore = AbilityScore.Ten;
+
   public ArmourClass: number = 10;
+
   public HitPoints: number = 5;
   public get IsDead(): boolean { return this.HitPoints <= 0; }
 
--- EverCraftKata/TypeScript/test/character.spec.ts
+++ EverCraftKata/TypeScript/test/character.spec.ts
@@ -1,4 +1,4 @@
-import { Character, Alignment } from '../src/index';
+import { Character, Alignment, AbilityScore } from '../src/index';
 
 describe('A character', () => {
 
@@ -23,5 +23,35 @@ describe('A character', () => {
     character.Alignment = alignment;
 
     expect(character.Alignment).toBe(alignment);
+  });
+
+  it('has a strength ability between 1 and 20, with a default of 10 ', ()=> {
+
+    expect(new Character().Strength).toBe(AbilityScore.Ten);
+  });
+
+  it('has a dexterity ability between 1 and 20, with a default of 10 ', ()=> {
+
+    expect(new Character().Dexterity).toBe(AbilityScore.Ten);
+  });
+
+  it('has a constitution ability between 1 and 20, with a default of 10 ', ()=> {
+
+    expect(new Character().Constitution).toBe(AbilityScore.Ten);
+  });
+
+  it('has a wisdom ability between 1 and 20, with a default of 10 ', ()=> {
+
+    expect(new Character().Wisdom).toBe(AbilityScore.Ten);
+  });
+
+  it('has a intelligence ability between 1 and 20, with a default of 10 ', ()=> {
+
+    expect(new Character().Intelligence).toBe(AbilityScore.Ten);
+  });
+
+  it('has a charisma ability between 1 and 20, with a default of 10 ', ()=> {
+
+    expect(new Character().Charisma).toBe(AbilityScore.Ten);
   });
 });
